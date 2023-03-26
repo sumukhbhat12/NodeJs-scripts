1a067f2762115b516080a0a3a8936e0347ebbff7
sumukhbhat <sumukha.bhat@manipal.net>
2023-03-26 20:31:32 +0530

    removed SID and Auth Token from the code.

--- twilio-api-read-list.js
+++ twilio-api-read-list.js
@@ -1,8 +1,9 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Find your Account SID and Auth Token at twilio.com/console
 // and set the environment variables. See http://twil.io/secure
-// $Env:TWILIO_ACCOUNT_SID="AC03f9b927c2da8d0668ff8b4b3eca61b1"
-// $Env:TWILIO_AUTH_TOKEN="7b7579816308ce57d4aa3b4804c9bea9"
+//Set Environment variables in powershell before running the code
+//$Env:TWILIO_ACCOUNT_SID="you will get SID in Twilio console"
+//$Env:TWILIO_AUTH_TOKEN="This as well"
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
